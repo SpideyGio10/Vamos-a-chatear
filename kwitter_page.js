f529b72da64ea5ea53f89eb236f16b47e943b560
SpideyGio10 <104469485+SpideyGio10@users.noreply.github.com>
2023-04-13 12:27:43 -0600

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,23 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAL0OMi1CMKpqORiJozMNETInEEmFYZxOs",
+    authDomain: "project-c94-d9c5d.firebaseapp.com",
+    databaseURL: "https://project-c94-d9c5d-default-rtdb.firebaseio.com",
+    projectId: "project-c94-d9c5d",
+    storageBucket: "project-c94-d9c5d.appspot.com",
+    messagingSenderId: "563152523934",
+    appId: "1:563152523934:web:e073e8b819687b123ae18f" };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+    user_name=localStorage.getItem("user_name");
+    room_name=document.getElementById("room_name").value;
+
+    function send(){
+        msg=document.getElementById("msg").value;
+        firabase.database().ref(room_name).push({
+            name:user_name,
+            message:msg,
+            like:0
+        });
+        document.getElementById("msg").value="";
+    }
